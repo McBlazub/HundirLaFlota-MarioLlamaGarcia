fd627ebf3f06f826b49747bf34d56b4221eac490
UBU <UBU@CEE-AI-C11-S08>
2025-05-08 19:20:01 +0200

    Agregar archivos de proyecto.

---
+++ HundirLaFlota-MarioLlamaGarcia/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HundirLaFlota_MarioLlamaGarcia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HundirLaFlota-MarioLlamaGarcia/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HundirLaFlota_MarioLlamaGarcia
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
